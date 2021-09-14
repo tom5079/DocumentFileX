82a49322e9127a4a94e548e535f915e532a72afa
tom5079 <tom5079@naver.com>
2021-09-14 14:47:25 +0900

    Simple test app

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
     buildToolsVersion = "30.0.3"
 
     defaultConfig {
         applicationId = "xyz.quaver.io.sample"
-        minSdkVersion(21)
-        targetSdkVersion(30)
+        minSdk = 21
+        targetSdk = 30
         versionCode = 1
         versionName = project.version.toString()
 
@@ -23,13 +23,33 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.2"
+    }
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    implementation("androidx.activity:activity-compose:1.3.1")
+
+    implementation("androidx.compose.ui:ui:1.0.2")
+    implementation("androidx.compose.ui:ui-tooling:1.0.2")
+    implementation("androidx.compose.foundation:foundation:1.0.2")
+    implementation("androidx.compose.material:material:1.0.2")
+    implementation("androidx.compose.material:material-icons-core:1.0.2")
+    implementation("androidx.compose.material:material-icons-extended:1.0.2")
+    implementation("androidx.compose.animation:animation:1.0.2")
+    implementation("androidx.compose.runtime:runtime:1.0.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.2")
+    implementation("com.google.accompanist:accompanist-appcompat-theme:0.16.0")
+
     implementation(project(":library"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
