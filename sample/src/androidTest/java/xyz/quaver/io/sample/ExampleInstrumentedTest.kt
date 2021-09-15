ebbbaaa153092a5c95ecc6f696e655bc79aeecf4
tom5079 <tom5079@naver.com>
2021-09-15 11:15:28 +0900

    WIP

--- sample/src/androidTest/java/xyz/quaver/io/sample/ExampleInstrumentedTest.kt
+++ sample/src/androidTest/java/xyz/quaver/io/sample/ExampleInstrumentedTest.kt
@@ -1,24 +1,55 @@
 package xyz.quaver.io.sample
 
+import android.content.Context
+import android.content.Intent
+import android.widget.Button
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.Until
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+const val TEST_PACKAGE = "xyz.quaver.io.sample"
+const val LAUNCH_TIMEOUT = 5000L
+const val SELECTOR_PACKAGE = "com.android.documentsui"
+
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("xyz.quaver.io.sample", appContext.packageName)
+
+    @Before
+    fun obtainPermission() {
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val intent = context.packageManager.getLaunchIntentForPackage(TEST_PACKAGE)?.apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+        context.startActivity(intent)
+
+        device.wait(
+            Until.hasObject(By.pkg(TEST_PACKAGE)),
+            LAUNCH_TIMEOUT
+        )
+
+        device.findObject(UiSelector().className(Button::class.java)).click()
+
+        device.wait(
+            Until.hasObject(By.pkg(SELECTOR_PACKAGE)),
+            LAUNCH_TIMEOUT
+        )
     }
+
+    @Test
+    fun test() {
+        assert(true)
+    }
+
 }
